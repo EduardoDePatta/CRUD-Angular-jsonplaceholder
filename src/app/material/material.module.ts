967ed29de7b91bb926155247ea5c466fa7e9ae48
EduardoDePatta <eduardo.depatta@gmail.com>
2022-09-04 21:59:07 -0300

    Adicionada a funcionalidade de buscar por id

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 const MaterialComponents = [
@@ -16,6 +17,7 @@ const MaterialComponents = [
   MatDialogModule,
   MatFormFieldModule,
   MatInputModule,
+  MatSnackBarModule
 
 ]
 
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -6,7 +6,6 @@ import { ViewChild } from '@angular/core';
 import { Register } from 'src/app/models/Register';
 
 const ELEMENT_DATA: Register[] = [];
-var contador: Array<any> = [];
 
 @Component({
   selector: 'app-home',
@@ -19,15 +18,13 @@ export class HomeComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'email', 'phone', 'actions'];
 
   dataSource!: Register[];
-
+  inputId: number = 1;
 
   setPosts(): void {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((json) => {
         this.dataSource = json;
-        contador = this.dataSource;
-        console.log(contador.length)
       });
   }
 
@@ -35,8 +32,7 @@ export class HomeComponent implements OnInit {
     this.setPosts();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   openDialog(element: Register | null): void {
     const dialogRef = this.dialog.open(ElementDialogComponent, {
@@ -44,7 +40,6 @@ export class HomeComponent implements OnInit {
       data:
         element === null
           ? {
-
               id: null,
               name: '',
               email: '',
@@ -81,94 +76,16 @@ export class HomeComponent implements OnInit {
     this.openDialog(element);
   }
 
-
-
-
-}
-export { contador };
-/*
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatTable } from '@angular/material/table';
-import { ElementDialogComponent } from 'src/app/components/element-dialog/element-dialog.component';
-import { ViewChild } from '@angular/core';
-import { Register } from 'src/app/models/Register';
-
-const ELEMENT_DATA: Register[] = [];
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-})
-export class HomeComponent implements OnInit {
-  @ViewChild(MatTable)
-  table!: MatTable<any>;
-  displayedColumns: string[] = ['id', 'userId', 'title', 'body', 'actions'];
-
-  dataSource!: Register[];
-
-
-  setPosts(): void {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+  filterElement(id: number): void {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((response) => response.json())
-      .then((json) => {
-        this.dataSource = json;
-      });
+      .then((json) =>
+        alert(`
+      ID: ${json.id}
+      Name: ${json.name}
+      Email: ${json.email}
+      Phone: ${json.phone}
+      `)
+      );
   }
-
-  constructor(public dialog: MatDialog) {
-    this.setPosts();
-  }
-
-  ngOnInit(): void {
-  }
-
-  openDialog(element: Register | null): void {
-    const dialogRef = this.dialog.open(ElementDialogComponent, {
-      width: '300px',
-      data:
-        element === null
-          ? {
-              id: null,
-              userId: null,
-              title: '',
-              body: '',
-            }
-          : {
-              id: element.id,
-              userId: element.userId,
-              title: element.title,
-              body: element.body,
-            },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result !== undefined) {
-        if (this.dataSource.map((p) => p.id).includes(result.id)) {
-          this.dataSource[result.id - 1] = result;
-          this.table.renderRows();
-          alert('Registro Atualizado');
-        } else {
-          this.dataSource.push(result);
-          this.table.renderRows();
-          alert('Registro NÃO FOI Atualizado');
-        }
-      }
-    });
-  }
-
-  deleteElement(id: number): void {
-    this.dataSource = this.dataSource.filter((p) => p.id !== id);
-    alert(`Registro removido`);
-  }
-
-  editElement(element: Register): void {
-    this.openDialog(element);
-  }
-
-
-
 }
-
-*/
